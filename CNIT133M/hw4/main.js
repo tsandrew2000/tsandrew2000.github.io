b170ae385806cea2000788ead806dc3fdc3d389c
tsandrew2000 <48392462+tsandrew2000@users.noreply.github.com>
2020-11-30 23:59:54 -0800

    Add files via upload

---
+++ CNIT133M/hw4/main.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', async e =>{
+  if('serviceWorker' in navigator){
+      try{
+          navigator.serviceWorker.register('serveceWorker.js');
+          console.log("Service Worker is registered");
+      }
+      catch(error){
+          console.log("Service Worker is not registered");
+      }
+  }
+});
+
+let deferredPrompt;
+let btnAdd;
+//  = document.querySelector(".button");
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  btnAdd.style.display = "block";
+});
+
+btnAdd.addEventListener('click', (e) => {
+
+  if (deferredPrompt) {
+      deferredPrompt.prompt();
+
+  deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+      } else {
+          console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+  });
+}
+});
+
+
+window.addEventListener('appinstalled', (evt) => {
+
+  console.log('INSTALL: Success');
+});
